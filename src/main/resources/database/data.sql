391c05fad588216a41c798005c29f3abc14e45f2
hiroe-kk <sntarmi78@gmail.com>
2024-07-03 08:27:39 +0900

    サンプルユーザーdemo2を追加

--- src/main/resources/database/data.sql
+++ src/main/resources/database/data.sql
@@ -1,5 +1,7 @@
 DELETE FROM account;
 
+-- サンプルユーザー1  ユーザー名:demo パスワード: demo
+-- サンプルユーザー2  ユーザー名:demo2 パスワード: demo2
 INSERT INTO account
 (
   username,
@@ -12,5 +14,11 @@ VALUES
   'demo',
   '$2a$10$jFvyAAcsi7DGFidFRxWO3uFw5r2vnacdFGWyQiy0juoY6Hv.gjlNu',
   'Taro',
+  'Yamada'
+),
+(
+  'demo2',
+  '$2a$10$VFMR8k1Fzq.YhtaSzP.6NOSxgyL1KppgpeNgp60Bw3aZhHozs0zRO',
+  'Jiro',
   'Yamada'
 );
